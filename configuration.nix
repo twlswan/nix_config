cad3a6f4410bcb9fd6ef25f589927af4c1b2c745
twlswan <jacopodelvivo@gmail.com>
2026-01-07 22:10:05 +0100

    init

---
+++ configuration.nix
@@ -0,0 +1,248 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.blacklistedKernelModules = [ "ntfs3" ];
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Amsterdam";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Graphics & Hardware
+  hardware = {
+    graphics.enable32Bit = true;
+    bluetooth.enable = true;
+  };
+
+  # KDE
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  
+  # Audio
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # User Account
+  users.users.z890 = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "docker" "libvirtd" ];
+    packages = with pkgs; [ tree ];
+  };
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # List by default
+      zlib
+      zstd
+      stdenv.cc.cc
+      curl
+      openssl
+      attr
+      libssh
+      bzip2
+      libxml2
+      acl
+      libsodium
+      util-linux
+      xz
+      systemd
+
+      # My own additions
+      xorg.libXcomposite
+      xorg.libXtst
+      xorg.libXrandr
+      xorg.libXext
+      xorg.libX11
+      xorg.libXfixes
+      libGL
+      libva
+      pipewire
+      xorg.libxcb
+      xorg.libXdamage
+      xorg.libxshmfence
+      xorg.libXxf86vm
+      libelf
+
+      # Required
+      glib
+      gtk2
+
+      # Without these it silently fails
+      xorg.libXinerama
+      xorg.libXcursor
+      xorg.libXrender
+      xorg.libXScrnSaver
+      xorg.libXi
+      xorg.libSM
+      xorg.libICE
+      gnome2.GConf
+      nspr
+      nss
+      cups
+      libcap
+      SDL2
+      libusb1
+      dbus-glib
+      ffmpeg
+      # Only libraries are needed from those two
+      libudev0-shim
+
+      # needed to run unity
+      gtk3
+      icu
+      libnotify
+      gsettings-desktop-schemas
+      # https://github.com/NixOS/nixpkgs/issues/72282
+      # https://github.com/NixOS/nixpkgs/blob/2e87260fafdd3d18aa1719246fd704b35e55b0f2/pkgs/applications/misc/joplin-desktop/default.nix#L16
+      # log in /home/leo/.config/unity3d/Editor.log
+      # it will segfault when opening files if you don’t do:
+      # export XDG_DATA_DIRS=/nix/store/0nfsywbk0qml4faa7sk3sdfmbd85b7ra-gsettings-desktop-schemas-43.0/share/gsettings-schemas/gsettings-desktop-schemas-43.0:/nix/store/rkscn1raa3x850zq7jp9q3j5ghcf6zi2-gtk+3-3.24.35/share/gsettings-schemas/gtk+3-3.24.35/:$XDG_DATA_DIRS
+      # other issue: (Unity:377230): GLib-GIO-CRITICAL **: 21:09:04.706: g_dbus_proxy_call_sync_internal: assertion 'G_IS_DBUS_PROXY (proxy)' failed
+
+      # Verified games requirements
+      xorg.libXt
+      xorg.libXmu
+      libogg
+      libvorbis
+      SDL
+      SDL2_image
+      glew110
+      libidn
+      tbb
+
+      # Other things from runtime
+      flac
+      freeglut
+      libjpeg
+      libpng
+      libpng12
+      libsamplerate
+      libmikmod
+      libtheora
+      libtiff
+      pixman
+      speex
+      SDL_image
+      SDL_ttf
+      SDL_mixer
+      SDL2_ttf
+      SDL2_mixer
+      libappindicator-gtk2
+      libdbusmenu-gtk2
+      libindicator-gtk2
+      libcaca
+      libcanberra
+      libgcrypt
+      libvpx
+      librsvg
+      xorg.libXft
+      libvdpau
+      # ...
+      # Some more libraries that I needed to run programs
+      pango
+      cairo
+      atk
+      gdk-pixbuf
+      fontconfig
+      freetype
+      dbus
+      alsa-lib
+      expat
+      # Needed for electron
+      libdrm
+      mesa
+      libxkbcommon
+      # Needed to run, via virtualenv + pip, matplotlib & tikzplotlib
+      stdenv.cc.cc.lib # to provide libstdc++.so.6
+      pkgs.gcc-unwrapped.lib # maybe only the first one needed
+
+      # needed to run appimages
+      fuse # needed for musescore 4.2.1 appimage
+      e2fsprogs # needed for musescore 4.2.1 appimage
+      fribidi # needed for xournalpp (nightly 08/11/2024)
+      librsvg # needed for xournalpp (nightly)
+      ibus # needed for xournalpp (nightly)
+    ];
+  };
+
+  virtualisation.libvirtd = {
+    enable = true;
+    #qemu = {
+    #  package = pkgs.qemu_kvm;
+    #  runAsRoot = true;
+    #  swtpm.enable = true;
+    #};
+  };
+
+  virtualisation.docker.enable = true;
+
+
+  # Programs (Modules)
+  programs = {
+    firefox.enable = true;
+    git.enable = true;
+    steam = {
+      enable = true;
+      protontricks.enable = true;
+    };
+    virt-manager.enable = true;
+  };
+  
+
+  # System Packages
+  environment.systemPackages = with pkgs; [
+    curl
+    wget
+    uv
+    mplayer
+    vlc
+
+    #ntfs
+    ntfs3g
+
+    # Wine 
+    wineWowPackages.waylandFull
+    winetricks
+    lutris
+
+    # Container/VM tools
+    distrobox
+
+
+    # platform tools
+    android-tools
+
+    jetbrains.clion
+    jetbrains.goland
+    vscode
+
+    kdePackages.partitionmanager
+    libgcc
+    gcc
+
+    google-chrome
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+
+    godot
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" ];
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "25.11"; 
+}
